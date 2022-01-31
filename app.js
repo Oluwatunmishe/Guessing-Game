4fcf639b1fde4e81e45e6b47a139f4d405c769fe
oluwatunmise <oluwatunmisefavour03>
2022-01-31 01:25:03 +0100

    add files

---
+++ app.js
@@ -0,0 +1,101 @@
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+const f1 = document.getElementById("f1");
+const f2 = document.getElementById("f2");
+const f3 = document.getElementById("f3");
+const f4 = document.getElementById("f4");
+const f5 = document.getElementById("f5");
+const f6 = document.getElementById("f6");
+
+document.getElementById("link1").addEventListener("click", function () {
+  myFunction(f1, f2, f3, f4, f5, f6);
+});
+
+function myFunction(f1, f2, f3, f4, f5, f6) {
+  const result = (f1.style.backgroundColor = "red");
+  const result2 = (f2.style.backgroundColor = "green");
+  const result3 = (f3.style.backgroundColor = "yellow");
+  const result4 = (f4.style.backgroundColor = "yellow");
+  const result5 = (f5.style.backgroundColor = "red");
+  const result6 = (f6.style.backgroundColor = "green");
+}
+
+document.getElementById("link3").addEventListener("click", function () {
+  myFunction(f1, f2, f3, f4, f5, f6);
+});
+
+function myFunction(f1, f2, f3, f4, f5, f6) {
+  const result = (f1.style.backgroundColor = "white");
+  const result2 = (f2.style.backgroundColor = "red");
+  const result3 = (f3.style.backgroundColor = "white");
+  const result4 = (f4.style.backgroundColor = "lemon");
+  const result5 = (f5.style.backgroundColor = "blue");
+  const result6 = (f6.style.backgroundColor = "white");
+}
+
+document.getElementById("f1").addEventListener("click", function () {
+  myFunction(f1, f2, f3, f4, f5, f6);
+});
+
+function myFunction(f1, f2, f3, f4, f5, f6) {
+  const result = (f1.style.backgroundColor = "green");
+  const result2 = (f2.style.backgroundColor = "red");
+  const result3 = (f3.style.backgroundColor = "purple");
+  const result4 = (f4.style.backgroundColor = "lemon");
+  const result5 = (f5.style.backgroundColor = "blue");
+  const result6 = (f6.style.backgroundColor = "white");
+}
+
+document.getElementById("link1").addEventListener("click", function () {
+  myFunction(f1, f2, f3, f4, f);
+});
+
+function myFunction(f1, f2, f3, f4, f5, f6) {
+  const result = (f1.style.backgroundColor = "green");
+  const result2 = (f2.style.backgroundColor = "red");
+  const result3 = (f3.style.backgroundColor = "purple");
+  const result4 = (f4.style.backgroundColor = "lemon");
+  const result5 = (f5.style.backgroundColor = "blue");
+  const result6 = (f6.style.backgroundColor = "white");
+}
+
+document.getElementById("f2").addEventListener("click", function () {
+  myFunction(f1, f2, f3, f4, f5, f6);
+});
+
+function myFunction(f1, f2, f3, f4, f5, f6) {
+  const result = (f1.style.backgroundColor = "white");
+  const result2 = (f2.style.backgroundColor = "yellow");
+  const result3 = (f3.style.backgroundColor = "purple");
+  const result4 = (f4.style.backgroundColor = "blue");
+  const result5 = (f5.style.backgroundColor = "red");
+  const result6 = (f6.style.backgroundColor = "white");
+}
+
+document.getElementById("f3").addEventListener("click", function () {
+  myFunction(f1, f2, f3, f4, f5, f6);
+});
+
+function myFunction(f1, f2, f3, f4, f5, f6) {
+  const result = (f1.style.backgroundColor = "yellow");
+  const result2 = (f2.style.backgroundColor = "red");
+  const result3 = (f3.style.backgroundColor = "blue");
+  const result4 = (f4.style.backgroundColor = "#fff");
+  const result5 = (f5.style.backgroundColor = "silver");
+  const result6 = (f6.style.backgroundColor = "green");
+}
+document.getElementById("f4").addEventListener("click", function () {
+  myFunction(f1, f2, f3, f4, f5, f6);
+});
+
+function myFunction(f1, f2, f3, f4, f5, f6) {
+  const result = (f1.style.backgroundColor = "yellow");
+  const result2 = (f2.style.backgroundColor = "white");
+  const result3 = (f3.style.backgroundColor = "yellow");
+  const result4 = (f4.style.backgroundColor = "red");
+  const result5 = (f5.style.backgroundColor = "yellow");
+  const result6 = (f6.style.backgroundColor = "blue");
+}
+
+
